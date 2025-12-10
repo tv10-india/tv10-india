83c60587e6401481e891d8dd19bdda9a18da52d2
Vedadnt <vedantshuklaji15@gmail.com>
2025-12-10 22:11:51 +0530

    Add missing sanity schema and structure files

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,16 +1,18 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
+import {apiVersion, dataset, projectId} from './env'
 import {schema} from './schemaTypes'
+import {structure} from './structure'
 
 export default defineConfig({
   name: 'default',
   title: 'tv10-news',
 
-  projectId: 'uh81euwc',
-  dataset: 'production',
+  projectId,
+  dataset,
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool({structure}), visionTool({defaultApiVersion: apiVersion})],
 
   schema,
 })
