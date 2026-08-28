b73f8d9cf210f215851877936593298434ddfb50
Vedant Shukla <157095022+tv10-india@users.noreply.github.com>
2026-08-28 09:23:24 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="hi">
       <body className={inter.className}>
         <GoogleTranslateScript />
 
